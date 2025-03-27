b6a1761fa8e5e5f70bfd12db743cd97520589904
ONieto7 <oscarniro@gmail.com>
2025-03-27 12:48:12 +0100

    Incluir fuentes y variables SCSS

--- Festival_Musica/gulpfile.js
+++ Festival_Musica/gulpfile.js
@@ -1,17 +1,27 @@
-import { src, dest, watch } from 'gulp'
+import { src, dest, watch, series } from 'gulp'
 import * as dartSass from 'sass'
 import gulpSass from 'gulp-sass'
 
 const sass = gulpSass(dartSass)
 
-export function css( done ){
-    src('src/scss/app.scss')
-        .pipe( sass() )
-        .pipe( dest('buildnpm/css') )
+export function js( done ) {
+    src('src/js/app.js')
+        .pipe( dest('build/js') ) 
 
     done()
 }
 
-export function dev( done ){
-    watch('src/scss/app.scss', css)
+export function css( done ) {
+    src('src/scss/app.scss', {sourcemaps: true})
+        .pipe( sass().on('error', sass.logError) )
+        .pipe( dest('build/css', {sourcemaps: '.'}) )
+
+    done()
 }
+
+export function dev() {
+    watch('src/scss/**/*.scss', css)
+    watch('src/js/**/*.js', js)
+}
+
+export default series( js, css, dev )
